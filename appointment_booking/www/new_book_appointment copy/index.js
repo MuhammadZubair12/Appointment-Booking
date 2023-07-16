5c15afe1a1199a76a188b5e1a701f45559eae50d
developers@techstation.com.eg <developers@techstation.com.eg>
2023-07-16 12:10:41 +0000

    New Theme for appointment

--- appointment_booking/www/new_book_appointment copy/index.js
+++ appointment_booking/www/new_book_appointment copy/index.js
@@ -115,79 +115,6 @@ async function services() {
             // console.log("Service", slot)
             pa.innerHTML += ` 
             {% set currencysymbal = frappe.db.get_single_value('Global Defaults', 'default_currency') %}
-           
-                    <div class="col-md-4 col-sm-6 princing-item green" style="margin-bottom: 7%">
-                        <div class="pricing-divider ">
-                            <h3 class="text-light">${slot.name}</h3>
-            
-                            <svg class='pricing-divider-img' enable-background='new 0 0 300 100' height='100px' id='Layer_1'
-                                preserveAspectRatio='none' version='1.1' viewBox='0 0 300 100' width='300px' x='0px'
-                                xml:space='preserve' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns='http://www.w3.org/2000/svg'
-                                y='0px'>
-                                <path class='deco-layer deco-layer--1' d='M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729
-            c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z' fill='#FFFFFF' opacity='0.6'></path>
-                                <path class='deco-layer deco-layer--2' d='M-35.667,44.628c0,0,42.91-34.463,87.51-14.191c77.31,35.141,113.304-1.952,146.639-4.729
-            c48.653-4.055,69.939,16.218,69.939,16.218v54.396H-35.667V44.628z' fill='#FFFFFF' opacity='0.6'></path>
-                                <path class='deco-layer deco-layer--3' d='M43.415,98.342c0,0,48.283-68.927,109.133-68.927c65.886,0,97.983,67.914,97.983,67.914v3.716
-            H42.401L43.415,98.342z' fill='#FFFFFF' opacity='0.7'></path>
-                                <path class='deco-layer deco-layer--4' d='M-34.667,62.998c0,0,56-45.667,120.316-27.839C167.484,57.842,197,41.332,232.286,30.428
-            c53.07-16.399,104.047,36.903,104.047,36.903l1.333,36.667l-372-2.954L-34.667,62.998z' fill='#FFFFFF'></path>
-                            </svg>
-                        </div>
-                        <div class="card-body bg-white mt-0 shadow" style="border-bottom-left-radius: 4% !important;border-bottom-right-radius: 4% !important;">
-                            <ul class="list-unstyled mb-5 position-relative" style="font-size: 12px;margin-bottom: 10% !important;">
-                                <li><b>Duration</b> ${slot.duration}</li>
-                                <li><b>Consultant</b> ${slot.service_provider}</li>
-                                <li><b>Price </b>  ${slot.price}<span style="color: #3096f3;
-                                font-weight: bold;
-                                margin-left: 2px;">{{currencysymbal}}<span></li>
-                                
-                            </ul>
-                            <div class="price-value" id="pack-price" onclick="onClick(event)">
-
-                
-                            <button type="button" id="btn_ss" class="btn btn-lg btn-block btn-custom" onclick="package(${JSON.stringify(slot).split('"').join("&quot;")})">Buy
-                            </button>
-                          
-                            </div>
-                            
-                        </div>
-                    </div>
-                
-
-
-
-
-
-
-
-        `
-        });
-    }
-   
-
-}
-// function myFunction(x) {
-//     x.classList.toggle("fa-check");
-//   }
-async function services_orignal() {
-    var select = document.getElementById('avail-service');
-    var value = select.options[select.selectedIndex].value;
-
-    if (value == "Select") {
-        let pa = document.getElementById('abc');
-        pa.innerHTML = ''
-    } else {
-        window.data = await get_avail_services(value)
-        let pa = document.getElementById('abc');
-        pa.innerHTML = ''
-        window.data.forEach((slot, index) => {
-       
-
-            // Get and append timeslot div
-            // console.log("Service", slot)
-            pa.innerHTML += ` 
-            {% set currencysymbal = frappe.db.get_single_value('Global Defaults', 'default_currency') %}
             <div class="col-md-4 col-sm-6" style="    margin-bottom: 7%;
             ">
             <div class="pricingTable">
@@ -217,21 +144,15 @@ async function services_orignal() {
 function onClick(e){
     e.preventDefault()
     let a = document.querySelector('.active');
-    let btnactive = document.querySelector('.btnactive');
     if (a) {
         a.classList.remove('active')
     } else {}
-    if (btnactive) {
-        a.classList.remove('btnactive')
-    } else {}
     // document.querySelector('.active').classList.remove('active')
     e.target.classList.add('active')
-    e.target.classList.add('btnactive')
     show_next_page()
 }
 
 function package(slot) {
-    var btn_ss = document.getElementById('btn_ss');
     let da = document.getElementById('duration-value');
     let na = document.getElementById('name-value');
     let pika = document.getElementById('pika');
